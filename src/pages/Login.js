dc7bc08e638f7937a9f7f53284f709bcb735a988
timmwrite <timmndirangu1@gmail.com>
2019-02-04 00:54:01 +0300

    src contains two directories; components and pages where pages contains the Login.js file

---
+++ src/pages/Login.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {
+    StyleSheet, 
+    Text, 
+    View, 
+    StatusBar} 
+    
+    from 'react-native';
+
+export default class Login extends Component<[]> {
+    render() {
+        return(
+            <View>
+                <Text>Login</Text>
+            </View>
+        )
+    } 
+}
